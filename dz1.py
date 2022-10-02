4450852ffe50d1bffe39c85b69d48cd93427a71c
Роман Бухмистов <rrb13@mail.ru>
2022-10-02 20:42:09 +0300

    решена 1 задача

---
+++ dz1.py
@@ -0,0 +1,28 @@
+'''Задача 1 Задайте список из нескольких чисел. 
+Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+*Пример:*
+- [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12'''
+import os
+clear = lambda: os.system('clear')
+clear()
+
+from random import randint
+
+def sum_randome(list_len):
+    a,b = 0,9
+    lst = [None] * list_len
+    for i in range(list_len):
+        lst[i] = randint(a,b)
+    lst
+    print(lst)
+    c=0
+    for i in range(1, len(lst) , 2):
+        a=lst[i]
+        c+=a
+    print("->", c)
+
+try:
+    lit_length=int(input("\nВведите целое число случайных чисел: "))
+    sum_randome(lit_length)
+except:
+    print("\nНужно вводить число!")
